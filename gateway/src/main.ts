72607a1deba87c4396412e079bb777505782b278
Vladlen <ilusenkovladlen@gmail.com>
2024-05-08 01:22:16 +0300

    disabled body-parser

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  const app = await NestFactory.create(AppModule, {
+    bodyParser: false,
+  });
   app.enableCors();
 
   const config = new DocumentBuilder()
