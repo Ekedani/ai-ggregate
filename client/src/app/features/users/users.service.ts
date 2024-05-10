a9425da50cd7336765805c3184fecd42044525fa
Vladlen <ilusenkovladlen@gmail.com>
2024-05-10 12:15:14 +0300

    add users dashobard

--- client/src/app/features/users/users.service.ts
+++ client/src/app/features/users/users.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {User} from "./interfaces/user";
+import {BehaviorSubject, tap} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+  private usersBehaviorSubject = new BehaviorSubject<User[]>([]);
+  public users$ = this.usersBehaviorSubject.asObservable();
 
-  constructor() { }
+  apiUrl = 'http://localhost:80';
+
+  constructor(private readonly httpClient: HttpClient) {
+  }
+
+  getUsers() {
+    return this.httpClient.get<{ users: User[] }>(`${this.apiUrl}/users`).pipe(
+      tap((data) => {
+          this.usersBehaviorSubject.next(data.users)
+        }
+      )
+    )
+  }
+
+  getUser(id: string) {
+    return this.httpClient.get<User>(`${this.apiUrl}/users/${id}`)
+  }
 }
