ae58dd9f91ccce104c03d6510ab72d1fe2037c45
Vladlen <ilusenkovladlen@gmail.com>
2024-05-20 18:13:20 +0300

    fix midjourney-data-fetcher.ts
    Signed-off-by: Vladlen <ilusenkovladlen@gmail.com>

--- aggregation/src/data-fetchers/strategies/images/midjourney-data-fetcher.ts
+++ aggregation/src/data-fetchers/strategies/images/midjourney-data-fetcher.ts
@@ -17,6 +17,7 @@ export class MidjourneyDataFetcher implements ImageFetcher {
   public async fetchData(): Promise<AiGeneratedImage[]> {
     puppeteer.use(StealthPlugin());
     const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
       headless: true,
     });
     const page = await browser.newPage();
